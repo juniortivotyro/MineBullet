1cdbeafd5fda85fa1688e2be932b97b675ecb106
Chris <github@cdhoff.com>
2017-11-06 16:15:31 -0600

    Send Log Message and Join Message to Player

--- src/com/cdhoff/MineBullet/MineBullet.java
+++ src/com/cdhoff/MineBullet/MineBullet.java
@@ -1,13 +1,11 @@
 package com.cdhoff.MineBullet;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.List;
 import java.util.logging.Level;
 
 public class MineBullet extends JavaPlugin implements Listener{
