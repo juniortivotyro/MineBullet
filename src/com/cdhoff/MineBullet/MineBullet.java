e5a783c4e31107637f50adbc935c7caa9d54f243
Chris <github@cdhoff.com>
2017-11-07 08:44:37 -0600

    Added Config and got Pushbullet integration working properly

--- src/com/cdhoff/MineBullet/MineBullet.java
+++ src/com/cdhoff/MineBullet/MineBullet.java
@@ -1,23 +1,46 @@
 package com.cdhoff.MineBullet;
 
 import com.github.sheigutn.pushbullet.Pushbullet;
+import com.github.sheigutn.pushbullet.items.push.sendable.SendablePush;
+import com.github.sheigutn.pushbullet.items.push.sendable.defaults.SendableNotePush;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.util.logging.Level;
 
-public class MineBullet extends JavaPlugin implements Listener{
+public class MineBullet extends JavaPlugin implements Listener {
 
-    private String apiToken = "o.KscEoYBLXucrfbM0zdokuyVKi7QglIwd";
+    private String apiToken = this.getConfig().getString("token");
 
-    @Override
-    public void onEnable(){
-        getLogger().info("Plugin Enabled 1");
-        getServer().getPluginManager().registerEvents(this, this);
+    private void createConfig() {
+
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdirs();
+        }
+        File file = new File(getDataFolder(), "config.yml");
+        if (!file.exists()) {
+            getLogger().info("Config.yml not found, creating!");
+            saveDefaultConfig();
+        } else {
+            getLogger().info("Config.yml found, loading!");
+        }
+
+
     }
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin Enabled");
+        getServer().getPluginManager().registerEvents(this, this);
+        createConfig();
+    }
+
+
 
     @Override
     public void onDisable(){
@@ -25,15 +48,35 @@ public class MineBullet extends JavaPlugin implements Listener{
     }
     
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event)
+    public void onPlayerJoin(PlayerJoinEvent joinEvent)
     {
-        Player player = event.getPlayer();
+        if(this.getConfig().getBoolean("Push when player joins the game?") == true) {
+        Player player = joinEvent.getPlayer();
         player.sendMessage("Hello, " + player.getName() + ". This is MineBullet testing.");
-        getLogger().log(Level.INFO, "Player Has Joined");
-            
+        getLogger().log(Level.INFO, player.getName() + " Has Joined");
+
         Pushbullet pushbullet = new Pushbullet(this.apiToken);
-        SendablePush note = SendableNotePush("Minecraft", player.getName() + " has joined."); 
-        pushbullet.push(note);
+        SendablePush note = new SendableNotePush("Minecraft", player.getName() + " has joined.");
+
+        try {
+            pushbullet.push(note);
+        } catch (Exception e) {
+            //getLogger().log(Level.SEVERE, ChatColor.RED + "An error has occured while trying to send the push. Have you updated the confing file yet?");
+            getServer().getConsoleSender().sendMessage(ChatColor.RED + "An error has occured while trying to send the push. Have you updated the confing file yet?");
+        }
+    }
+    }
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent leaveEvent) {
+        if (this.getConfig().getBoolean("Push when player leaves the game?") == true) {
+            Player player = leaveEvent.getPlayer();
+            player.sendMessage("Hello, " + player.getName() + ". This is MineBullet testing.");
+            getLogger().log(Level.INFO, player.getName() + " Has Left");
+
+            Pushbullet pushbullet = new Pushbullet(this.apiToken);
+            SendablePush note = new SendableNotePush("Minecraft", player.getName() + " has left.");
+            pushbullet.pushToAllDevices(note);
+        }
     }
 
 }
